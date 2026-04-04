99e0e40c24d6737198d01eaf02415176d0f0a2d6
Onlysuuji <onlysuuji@outlook.jp>
2026-04-05 02:33:28 +0900

    Add YouTube import and search workflow

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Player from "./player";
 export default function Page() {
   return (
     <main className="p-4">
-      <h1>Video + Audio Offset Player</h1>
       <Player />
     </main>
   );
